35ec563e438210a7fc4a23db0750588c06cc95a5
Israa Almahmoud <99071791@mydavinci.nl>
2023-02-09 10:34:26 +0100

    opdracht 1

--- style.js
+++ style.js
@@ -6,12 +6,36 @@ function clicked_button(event){
     console.log('clicked button');
     var div = document.getElementById("result");
     var name = prompt('Wat is je naam')
-    div.innerHTML = 'Hallo vanuit de button ...';
+    div.innerHTML = 'Hallo vanuit de button ...' ; //+ name
     }
 
 buttom = document.getElementById("start")
 
 buttom.onclick = clicked_button
-
-
 //clicked_button(1)
+
+
+
+// var list = [2,5,'alles is liefde',2,3];
+// for (let index in list){
+//     var element = list[index];
+//     console.log(element);}
+
+// for (let element of list){
+//     console.log(element);}
+
+// for (let index = 0; index < list.length; index++){
+//     var element = list[index];
+//     console.log(element);}
+
+// let index = 0;while (index < list.length){
+//     var element = list[index];
+//     console.log(element);
+//     index++; } // index = index + 1
+
+
+
+
+// var basis = 7;for (let index = 1; index <= 10; index++){
+//     let line = index.toString() + ' x ' + basis.toString() + ' = ' + (index * basis).toString()
+//     console.log(line);}
