c68a81a427222ee0e5de0724b50a62ae9d9b5e2d
Israa Almahmoud <99071791@mydavinci.nl>
2023-03-09 10:35:33 +0100

    horeca aap verbetert en opnieuw gemaakt

--- moudle_01.JavaScript-Crash-Course/horeca.js
+++ moudle_01.JavaScript-Crash-Course/horeca.js
@@ -1,39 +1,49 @@
 function horeca_app(){
     //lists
+    var bon = " ";
     const dranks = {
         "bier" : 2.50,
         "wijn" : 3.75,
         "fris" : 1.65
     };
-    const bon = [];
 
     //var
-    let testen = true
-    var totaalPrijs = 0
+    let testen = true;
+
+    var totaalPrijs = 0;
+
+    var brierAmount = 0;
+    var wijnAmount = 0;
+    var frisAmount = 0;
 
     while (testen) {
         var keuze = prompt("Wat wilt u bestellen?  ");
         if (keuze in dranks){
             var aantal = parseInt(prompt("Hoeveel drankje wilt u hebben?  "));
-            if (bon.includes(keuze)){
-                console.log("hello")
-                bon["amount"] += aantal
-            }else{
-            bon.push({
-                "name" : keuze,
-                "price" : dranks[keuze],
-                "amount" : aantal
-            });
-            };
-            totaalPrijs += dranks[keuze] * aantal
+            if (keuze.toLowerCase() == "bier"){
+                brierAmount += aantal
+            } else if (keuze.toLowerCase() == "wijn"){
+                wijnAmount += aantal
+            } else{
+                frisAmount += aantal
+            } 
         } else if (keuze.toLowerCase() == 'stop'){
+            if (brierAmount > 0){
+                bon += ("Name: bier "+ "Price: 2.50 " + "Amount: " + brierAmount);
+                totaalPrijs += (2.50 * brierAmount);
+            } if (wijnAmount > 0){
+                bon += ("\nName: wijn "+ "Price: 3.75 " + "Amount: " + wijnAmount);
+                totaalPrijs += (3.75 * wijnAmount);
+            } if (frisAmount > 0){
+                bon += ("\nName: fris "+ "Price: 1.65 " + "Amount: " + frisAmount);
+                totaalPrijs += (1.65 * frisAmount);
+            }
+            bon += ("\nTotaal prijs =  " + totaalPrijs)
             testen = false
         } else {
             alert("Sorry, Dat ken ik niet")   
-        }   
+        }
     }
-    bon.push({"Totaalprijs" : totaalPrijs})
     return bon
 }
-
 console.log(horeca_app())
