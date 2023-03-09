562d12db1e86b718763ff9f615df79261f45d7f1
Israa Almahmoud <99071791@mydavinci.nl>
2023-03-09 16:26:52 +0100

    horeca aap verbetert

--- moudle_01.JavaScript-Crash-Course/horeca.js
+++ moudle_01.JavaScript-Crash-Course/horeca.js
@@ -1,22 +1,14 @@
 function horeca_app(){
     //lists
-    var bon = " ";
     const dranks = {
         "bier" : 2.50,
         "wijn" : 3.75,
         "fris" : 1.65
     };
-
     //var
-    let testen = true;
+    let meer_bestellen = true;
 
-    var totaalPrijs = 0;
-
-    var brierAmount = 0;
-    var wijnAmount = 0;
-    var frisAmount = 0;
-
-    while (testen) {
+    while (meer_bestellen) {
         var keuze = prompt("Wat wilt u bestellen?  ");
         if (keuze in dranks){
             var aantal = parseInt(prompt("Hoeveel drankje wilt u hebben?  "));
@@ -28,22 +20,26 @@ function horeca_app(){
                 frisAmount += aantal
             } 
         } else if (keuze.toLowerCase() == 'stop'){
-            if (brierAmount > 0){
-                bon += ("Name: bier "+ "Price: 2.50 " + "Amount: " + brierAmount);
-                totaalPrijs += (2.50 * brierAmount);
-            } if (wijnAmount > 0){
-                bon += ("\nName: wijn "+ "Price: 3.75 " + "Amount: " + wijnAmount);
-                totaalPrijs += (3.75 * wijnAmount);
-            } if (frisAmount > 0){
-                bon += ("\nName: fris "+ "Price: 1.65 " + "Amount: " + frisAmount);
-                totaalPrijs += (1.65 * frisAmount);
-            }
-            bon += ("\nTotaal prijs =  " + totaalPrijs)
-            testen = false
+            meer_bestellen = false
         } else {
             alert("Sorry, Dat ken ik niet")   
         }
     }
     return bon
 }
-console.log(horeca_app())
+function bon(brierAmount , wijnAmount , frisAmount , totaalPrijs ){
+    if (brierAmount > 0){
+        bon += ("Name: bier "+ "Price: 2.50 " + "Amount: " + brierAmount);
+        totaalPrijs += (2.50 * brierAmount);
+    } if (wijnAmount > 0){
+        bon += ("\nName: wijn "+ "Price: 3.75 " + "Amount: " + wijnAmount);
+        totaalPrijs += (3.75 * wijnAmount);
+    } if (frisAmount > 0){
+        bon += ("\nName: fris "+ "Price: 1.65 " + "Amount: " + frisAmount);
+        totaalPrijs += (1.65 * frisAmount);
+    }
+    bon += ("\nTotaal prijs =  " + totaalPrijs)
+    return bon
+}
+
+function 
