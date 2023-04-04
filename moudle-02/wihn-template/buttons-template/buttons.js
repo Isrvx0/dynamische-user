537bb4319dfd29599f1a8d78e14fbaafa875da77
Israa Almahmoud <99071791@mydavinci.nl>
2023-04-04 23:29:24 +0200

    button opdracht (1)

---
+++ moudle-02/wihn-template/buttons-template/buttons.js
@@ -0,0 +1,15 @@
+const img = document.getElementById('image1');
+const img2 = document.getElementById('image2');
+
+function changeImage1(){
+    img.src = 'images/bg1.jpg';
+    img2.src = 'images/1.jpg';
+}
+function changeImage2(){
+  img.src = 'images/bg2.jpg';
+  img2.src = 'images/2.jpg';
+}
+function changeImage3(){
+  img.src = 'images/bg3.jpg';
+  img2.src = 'images/3.jpg';
+}
