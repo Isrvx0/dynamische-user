a12974cb9fb1fc604d1ccd057ef5f4109e0b84b0
Israa Almahmoud <99071791@mydavinci.nl>
2023-05-10 13:30:47 +0200

    update 'transform'

--- moudle-02/MoveTank/tank.js
+++ moudle-02/MoveTank/tank.js
@@ -8,14 +8,24 @@ function checkKey(e) {
     e = e || window.event;
     if(e.keyCode == 32){
     	console.log("spacebar");
+        image.style.transform = "rotate(90deg)"
+
     } else if (e.keyCode == '38') {  // up arrow
         console.log("Up arrow");
+        image.style.transform = "rotate(360deg)"
+
     } else if (e.keyCode == '40') { // down arrow
         console.log("down arrow");
+        image.style.transform = "rotate(180deg)"
+
     } else if (e.keyCode == '37') { // left arrow
     	console.log("left arrow");
+        image.style.transform = "rotate(-90deg)"
+
     } else if (e.keyCode == '39') {   // right arrow
     	console.log("right arrow");
     	image.style.backgroundPosition = `164px 0px`; // check goed de rupsband
+        image.style.transform = "rotate(90deg)"
+
     }
 }
