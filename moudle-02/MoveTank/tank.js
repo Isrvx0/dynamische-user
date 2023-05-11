d9cbedfebea027fc75692e43e03ebe452c399e5b
Israa Almahmoud <99071791@mydavinci.nl>
2023-05-11 10:55:57 +0200

    oplossing 'lukt niet'

--- moudle-02/MoveTank/tank.js
+++ moudle-02/MoveTank/tank.js
@@ -4,21 +4,24 @@ var demo = document.getElementById("demo");
 document.onkeydown = checkKey;
 image.style.transform = "rotate(90deg)"
 let move = 0;
-let steps = 10;
+let step = 0;
+
 
 function checkKey(e) {
-    steps = steps + 10
     move = parseInt(move) + 164;
+    step = parseInt(step) + 10;
 	console.log("key nr = " + e.keyCode);
     e = e || window.event;
     if(e.keyCode == 32){
     	console.log("spacebar");
         image.style.transform = "rotate(90deg)"
         image.style.backgroundPosition = `-${parseInt(move)}px 0px`;
+  
     } else if (e.keyCode == '38') {  // up arrow
         console.log("Up arrow");
         image.style.transform = "rotate(360deg)"
         image.style.backgroundPosition = `-${parseInt(move)}px 0px`;
+
 
     } else if (e.keyCode == '40') { // down arrow
         console.log("down arrow");
@@ -31,9 +34,9 @@ function checkKey(e) {
         image.style.backgroundPosition = `-${parseInt(move)}px 0px`;
 
     } else if (e.keyCode == '39') {   // right arrow
-        console.log("right arrow");
+    	console.log("right arrow");
     	image.style.backgroundPosition = `${parseInt(move)}px 0px`; // check goed de rupsband
         image.style.transform = "rotate(90deg)"
-        demo.style.left = `${parseInt(steps)}px`;
+        demo.style.left = `${parseInt(step)}px`;
     }
 }
