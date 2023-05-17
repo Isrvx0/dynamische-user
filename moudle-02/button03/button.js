bcde78c4dc400d762829f8a9b8fd42ccca1b246d
Israa Almahmoud <99071791@mydavinci.nl>
2023-05-17 14:08:29 +0200

    update on assigment 2 ;)

--- moudle-02/button03/button.js
+++ moudle-02/button03/button.js
@@ -1,24 +1,42 @@
 const numbers= [5,10,15,20,25,30];  
-var width = (window.innerWidth - 100) / 11;
-var height = (window.innerHeight - 100) / 17;
+const container = document.getElementById("container")
 
-for(let number = 1; number < 31; number++){
-      let newBtn = document.createElement('button');
-      var lineBreak = document.createElement("br");
-     
-      newBtn.innerText= number;
-      document.querySelector('#container').appendChild(newBtn);
+var width = (window.innerWidth - 100) / 6;
+var height = (window.innerHeight - 100) / 5;
+var clicks = 0;
 
-      for (num in numbers){
-        if (number == numbers[num]){
-            document.querySelector('#container').appendChild(lineBreak);
-          };
+var btn_num = parseInt(prompt("How many buttons do you want to create?  "));
+
+function createButton (button_numbers){
+  for(let number = 1; number < button_numbers +1; number++){
+        let newBtn = document.createElement('button');
+        var lineBreak = document.createElement("br");
+      
+        newBtn.innerText= number;
+        document.querySelector('#container').appendChild(newBtn);
+
+        for (num in numbers){
+          if (number == numbers[num]){
+              document.querySelector('#container').appendChild(lineBreak);
+            };
+        }
+      
+      //   style
+        newBtn.style.padding = `${height}px ${width}px`;
+        newBtn.style.margin = `5px`;
+        newBtn.style.backgroundColor = "green";
+        newBtn.style.color = "white";
+        newBtn.style.fontFamily= "Serif";
+        newBtn.style.fontSize = "45px"
+
       }
-    
-    //   style
-      newBtn.style.padding = `${height}px ${width}px`;
-      newBtn.style.margin = `5px`;
-      newBtn.style.backgroundColor = "green";
-      newBtn.style.color = "white";
-      newBtn.style.fontFamily= "Serif";
-    }
+    };
+
+createButton(btn_num)
+
+
+
+// container.addEventListener("click", function() { 
+//   console.log(this.innerText + "You clicked me");
+//   clicks += 1;
+// });
