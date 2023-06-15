32de4fdb17a577291b6cb997e46a7ee7cffd827e
Israa Almahmoud <99071791@mydavinci.nl>
2023-06-15 21:10:32 +0200

    move pionnen

--- meester_proef/mokshaPatamu.js
+++ meester_proef/mokshaPatamu.js
@@ -5,7 +5,7 @@ let snack_place = ["13", "30", "15", "24"]
 rollen_button.onclick = rollen;
 // score_button.onclick 
 help_button.onclick = help;
-
+let questionList = ["5", "17", "28", "44", "49"]
 // ------------------------------ Create the game board :
 function createCard (){
         for(let number = 1; number < 50 +1; number++){
@@ -28,8 +28,16 @@ function createCard (){
                 newBtn.style.fontSize = "15pt";
                 newBtn.style.textAlign = "center"; 
                 newBtn.style.verticalAlign = "middle";
-        
+                newBtn.style.position = "relative"
                 newBtn.onclick = create_pionnen;
+
+                for (element of questionList) {
+                        if (element == newBtn.className) {
+                                newBtn.style.background = `#DDA0DD`;
+                                newBtn.style.color = `white`;
+
+                        }
+                }
         }}
             
 // ------------------------------ Rollen :
@@ -82,6 +90,10 @@ function create_pionnen(e) {
                         this.style.background = `light${player_turn}`
                 }
                 
+                // for (element of questionList) {
+                //         if (element == class_name) {
+                //                 new_question;
+                //         }
                 winner(this_className , this.style.background , board_game);
                 
         } 
@@ -100,6 +112,78 @@ function winner(class_name , style , board_game) {
 
         }
 };
+
+// ------------------------------ Question function :
+
+// function question(e) {
+
+// };
+
+
+const movingElements = document.getElementsByClassName("moving");
+
+for (let i = 0; i < movingElements.length; i++) { // alle pionnen krijgen relative position
+
+  movingElements[i].style.position = "relative";
+  console.log(movingElements[i])
+}
+
+function filter(e) {
+  let target = e.target;
+
+  if (!target.classList.contains("moving")) { // if element class != "moving" , stopt de function
+    return;
+  }
+
+  target.moving = true; //otherwise return true en blijft lopen.
+
+  //Check if Mouse events exist on users' device
+  if (e.clientX) {
+    target.oldX = e.clientX; // If they exist then use Mouse input
+    target.oldY = e.clientY;
+  } 
+//   else { //-- >Otherwise use touch input
+//     target.oldX = e.touches[0].clientX; 
+//     target.oldY = e.touches[0].clientY;
+//   }
+
+  target.oldLeft = window.getComputedStyle(target).getPropertyValue('left').split('px')[0] * 1; // getComputedStyle = Het haalt de berekende CSS-eigenschappen en -waarden van een HTML-element op.
+  target.oldTop = window.getComputedStyle(target).getPropertyValue('top').split('px')[0] * 1; // getPropertyValue = returns the value of the specified CSS property.
+
+
+  document.onmousemove = dr; // It occurs when the pointer moves over an element.
+//   document.ontouchmove = dr; --> only for touch screen.
+
+  function dr(event) {
+    event.preventDefault(); // cancels the event if it is cancelable <Clicking on a link, prevent the link from following the URL>
+
+    if (!target.moving) {
+      return;
+    }
+
+    if (event.clientX) {
+      target.distX = event.clientX - target.oldX;
+      target.distY = event.clientY - target.oldY;
+    } 
+
+//     else { //-- >Otherwise use touch input
+//       target.distX = event.touches[0].clientX - target.oldX;
+//       target.distY = event.touches[0].clientY - target.oldY;
+//     }
+
+    target.style.left = target.oldLeft + target.distX + "px";
+    target.style.top = target.oldTop + target.distY + "px";
+  }
+
+  function endDrag() { // einde slepen --> stopt de function
+    target.moving = false; 
+  }
+  target.onmouseup = endDrag;
+  target.ontouchend = endDrag;
+}
+document.onmousedown = filter;
+document.ontouchstart = filter;
+
 
 createCard();
 
